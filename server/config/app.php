ed81c45f881517f51e0dd63d086c1b4489be1753
ishimigne_note <garethbale3113@gmail.com>
2020-12-12 15:40:03 +0900

    Imageクラスのプロパイダーからコメントアウト

--- server/config/app.php
+++ server/config/app.php
@@ -166,7 +166,7 @@ return [
          * Package Service Providers...
          */
 
-        Intervention\Image\ImageServiceProvider::class,
+        // Intervention\Image\ImageServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -228,7 +228,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
-        'Image' => Intervention\Image\ImageServiceProvider::class,
+        // 'Image' => Intervention\Image\ImageServiceProvider::class,
     ],
 
 ];
