79f483130e414f640c6c52c9ef4b525dbbfe91f0
ishimigne_note <garethbale3113@gmail.com>
2020-12-12 21:53:50 +0900

    ルーティングのグループ化

--- server/routes/web.php
+++ server/routes/web.php
@@ -23,23 +23,46 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/', 'UserController@index');
 
-//マイページルート
-Route::get('/sell', 'UserController@sell')->middleware('auth');
-Route::post('/sell', 'UserController@addSell')->middleware('auth');
-Route::get('/mypage', 'UserController@myPage')->middleware('auth');
-Route::get('/mypage/like', 'UserController@myLikes')->middleware('auth');
-Route::post('/mypage/like', 'UserController@addMylikes')->middleware('auth');
-Route::post('/mypage/like/delete', 'UserController@deleteMylikes')->middleware('auth');
-Route::get('/mypage/sell/now', 'UserController@sellNow')->middleware('auth');
-Route::get('/mypage/sell/trade', 'UserController@sellTrade')->middleware('auth');
-Route::get('/mypage/sell/end', 'UserController@sellEnd')->middleware('auth');
-Route::get('/mypage/bid/now', 'UserController@bidNow')->middleware('auth');
-Route::get('/mypage/bid/trade', 'UserController@bidTrade')->middleware('auth');
-Route::get('/mypage/bid/end', 'UserController@bidEnd')->middleware('auth');
+Route::group(['middleware' => 'auth'], function () {
 
-//プロフィールルート
-Route::get('/profile', 'UserController@profile')->middleware('auth');
-Route::get('/profile/send', 'UserController@send')->middleware('auth');
-Route::get('/profile/pay', 'UserController@pay')->middleware('auth');
-Route::get('/profile/mail', 'UserController@mail')->middleware('auth');
-Route::get('/profile/logout', 'UserController@logout')->middleware('auth');
+    Route::get('/sell', 'UserController@sell');
+    Route::post('/sell', 'UserController@addSell');
+    Route::get('/mypage', 'UserController@myPage');
+    Route::get('/mypage/like', 'UserController@myLikes');
+    Route::post('/mypage/like', 'UserController@addMylikes');
+    Route::post('/mypage/like/delete', 'UserController@deleteMylikes');
+    Route::get('/mypage/sell/now', 'UserController@sellNow');
+    Route::get('/mypage/sell/trade', 'UserController@sellTrade');
+    Route::get('/mypage/sell/end', 'UserController@sellEnd');
+    Route::get('/mypage/bid/now', 'UserController@bidNow');
+    Route::get('/mypage/bid/trade', 'UserController@bidTrade');
+    Route::get('/mypage/bid/end', 'UserController@bidEnd');
+
+    //プロフィールルート
+    Route::get('/profile', 'UserController@profile');
+    Route::get('/profile/send', 'UserController@send');
+    Route::get('/profile/pay', 'UserController@pay');
+    Route::get('/profile/mail', 'UserController@mail');
+    Route::get('/profile/logout', 'UserController@logout');
+});
+
+// //マイページルート
+// Route::get('/sell', 'UserController@sell')->middleware('auth');
+// Route::post('/sell', 'UserController@addSell')->middleware('auth');
+// Route::get('/mypage', 'UserController@myPage')->middleware('auth');
+// Route::get('/mypage/like', 'UserController@myLikes')->middleware('auth');
+// Route::post('/mypage/like', 'UserController@addMylikes')->middleware('auth');
+// Route::post('/mypage/like/delete', 'UserController@deleteMylikes')->middleware('auth');
+// Route::get('/mypage/sell/now', 'UserController@sellNow')->middleware('auth');
+// Route::get('/mypage/sell/trade', 'UserController@sellTrade')->middleware('auth');
+// Route::get('/mypage/sell/end', 'UserController@sellEnd')->middleware('auth');
+// Route::get('/mypage/bid/now', 'UserController@bidNow')->middleware('auth');
+// Route::get('/mypage/bid/trade', 'UserController@bidTrade')->middleware('auth');
+// Route::get('/mypage/bid/end', 'UserController@bidEnd')->middleware('auth');
+
+// //プロフィールルート
+// Route::get('/profile', 'UserController@profile')->middleware('auth');
+// Route::get('/profile/send', 'UserController@send')->middleware('auth');
+// Route::get('/profile/pay', 'UserController@pay')->middleware('auth');
+// Route::get('/profile/mail', 'UserController@mail')->middleware('auth');
+// Route::get('/profile/logout', 'UserController@logout')->middleware('auth');
