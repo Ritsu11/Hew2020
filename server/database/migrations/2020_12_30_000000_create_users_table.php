cacc3af4ec98d93e75503aec3faf38dcfd3a2aad
ishimigne_note <garethbale3113@gmail.com>
2020-12-14 00:04:51 +0900

    Userマイグレーションファイルの変更

--- server/database/migrations/2020_12_30_000000_create_users_table.php
+++ server/database/migrations/2020_12_30_000000_create_users_table.php
@@ -16,12 +16,28 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name', 64)->unique();
+
+            $table->string('firstname')->nullable();
+            $table->string('lastname')->nullable();
+            $table->string('firstname_kana')->nullable();
+            $table->string('lastname_kana')->nullable();
+            $table->string('postal_code')->nullable();
+
+            $table->bigInteger('area_id')->unsigned()->nullable();
+            $table->string('city')->nullable();
+            $table->string('city_address')->nullable();
+
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
             $table->timeTz('withdrawal_at')->nullable()->change();
+
+            //外部キー制約の設定
+            $table->foreign('area_id')
+                ->references('id')
+                ->on('areas');
         });
     }
 
