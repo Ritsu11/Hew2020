c5e25c04502059764f9006d1452c88d31c272f2e
ishimigne_note <garethbale3113@gmail.com>
2020-12-14 00:05:16 +0900

    不要なルートの削除

--- server/routes/web.php
+++ server/routes/web.php
@@ -46,24 +46,3 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('/profile/mail', 'UserController@mail');
     Route::get('/profile/logout', 'UserController@logout');
 });
-
-// //マイページルート
-// Route::get('/sell', 'UserController@sell')->middleware('auth');
-// Route::post('/sell', 'UserController@addSell')->middleware('auth');
-// Route::get('/mypage', 'UserController@myPage')->middleware('auth');
-// Route::get('/mypage/like', 'UserController@myLikes')->middleware('auth');
-// Route::post('/mypage/like', 'UserController@addMylikes')->middleware('auth');
-// Route::post('/mypage/like/delete', 'UserController@deleteMylikes')->middleware('auth');
-// Route::get('/mypage/sell/now', 'UserController@sellNow')->middleware('auth');
-// Route::get('/mypage/sell/trade', 'UserController@sellTrade')->middleware('auth');
-// Route::get('/mypage/sell/end', 'UserController@sellEnd')->middleware('auth');
-// Route::get('/mypage/bid/now', 'UserController@bidNow')->middleware('auth');
-// Route::get('/mypage/bid/trade', 'UserController@bidTrade')->middleware('auth');
-// Route::get('/mypage/bid/end', 'UserController@bidEnd')->middleware('auth');
-
-// //プロフィールルート
-// Route::get('/profile', 'UserController@profile')->middleware('auth');
-// Route::get('/profile/send', 'UserController@send')->middleware('auth');
-// Route::get('/profile/pay', 'UserController@pay')->middleware('auth');
-// Route::get('/profile/mail', 'UserController@mail')->middleware('auth');
-// Route::get('/profile/logout', 'UserController@logout')->middleware('auth');
